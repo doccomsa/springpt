e7b769e31e702a8eb2d14b5204e8bbc6f51f37b0
홍길동 <doccomsa@naver.com>
2020-10-14 16:05:12 +0900

    프로젝트 초기작업

---
+++ SejinMall/src/main/webapp/WEB-INF/views/js/join.js
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+ 
+ $(function(){
+  		
+  		$("#btnJoin").click(function(){
+  			
+  			
+  			alert("유효성검사");
+  			
+  			// 유효성 검사구문
+  			
+  			
+  			
+  			$("#frmJoin").submit();
+  			
+  			
+  			
+  		});
+  		
+ });
